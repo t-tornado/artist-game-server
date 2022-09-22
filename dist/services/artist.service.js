0ba234c51d3a86a44d360028647f2cce88e5575b
t-tornado <anthonyamp0011@gmail.com>
2022-09-22 12:05:35 +0000

    fix

--- dist/services/artist.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ArtistService = void 0;
-var ArtistService = /** @class */ (function () {
-    function ArtistService() {
-    }
-    return ArtistService;
-}());
-exports.ArtistService = ArtistService;
--- dist/util/functions/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.getRandomBtnRange = void 0;
-function getRandomBtnRange(max) {
-    return Math.floor(Math.random() * max);
-}
-exports.getRandomBtnRange = getRandomBtnRange;
